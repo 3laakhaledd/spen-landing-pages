95d5fe7c86283c1ac0d562ac7bd2fd296c41f291
3laakhaledd <95441080+3laakhaledd@users.noreply.github.com>
2026-09-21 21:48:00 +0300

    feat: init server + routing for SPEN landing pages

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/last-weekend', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'last-weekend.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`SPEN Landing Pages running on port ${PORT}`);
+});
